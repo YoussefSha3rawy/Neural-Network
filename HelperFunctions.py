Fixed critical bugs Modified softmax to account for large input values Modified cross-entropy loss Added derivative for ce loss Moved testing to notebook Made activations have their own layer Added batch training Added l1 and l2 Added decay and momentum

HelperFunctions.py
@@ -1,12 +1,19 @@
 from enum import Enum
 import numpy as np
 import numpy.typing as npt
+import sys
+
+
+class LossFunctionEnum(Enum):
+    MSE = 'MSE'
+    CCE = 'CCE'
 
 
 class ActivationFunctionEnum(Enum):
     RELU = 'RELU'
     SIGMOID = 'SIGMOID'
     SOFTMAX = 'SOFTMAX'
+    NONE = 'NONE'
 
 
 class ActivationFunctions:
@@ -17,7 +24,7 @@ class ActivationFunctions:
             return ActivationFunctions.softmax_activate(X)
         elif activation_function == ActivationFunctionEnum.SIGMOID:
             return ActivationFunctions.sigmoid_activate(X)
-        else:
+        elif activation_function == ActivationFunctionEnum.NONE:
             return X
 
     def activation_derivitive(a, activation_function: ActivationFunctionEnum):
@@ -27,7 +34,7 @@ class ActivationFunctions:
             return ActivationFunctions.softmax_derivative(a)
         elif activation_function == ActivationFunctionEnum.SIGMOID:
             return ActivationFunctions.sigmoid_derivative(a)
-        else:
+        elif activation_function == ActivationFunctionEnum.NONE:
             return np.ones(a.shape)
 
     def relU_activate(X):
@@ -37,16 +44,17 @@ class ActivationFunctions:
         return 1 / (1 + np.exp(-X))
 
     def softmax_activate(X):
-        return np.exp(X) / np.exp(X).sum()
+        exp_values = np.exp(X - np.max(X, axis=1, keepdims=True))
+        return exp_values / exp_values.sum(axis=1, keepdims=True)
 
     def sigmoid_derivative(a):
         return a * (1-a)
 
     def relU_derivative(a):
-        return (a >= 0) * 1
+        return (a > 0) * 1
 
     def softmax_derivative(a):
-        I = np.eye(a.shape[0])
+        I = np.eye(a.shape[-1])
         return np.dot((I - a.T), a)
 
 
@@ -57,12 +65,16 @@ class LossFunctions:
     def mean_squared_error(y_actual: npt.ArrayLike, y_pred: npt.ArrayLike) -> float:
         return np.mean(np.square(y_pred - y_actual))
 
-    def cross_entropy(y_actual: npt.ArrayLike, y_pred: npt.ArrayLike) -> float:
-        N = y_pred.shape[0]
-        ce = -np.sum(y_actual*np.log(y_pred))/N
+    def cross_entropy(targets, predictions, epsilon=1e-12):
+        predictions = np.clip(predictions, epsilon, 1. - epsilon)
+        ce = -np.mean(targets*np.log(predictions+1e-9))
         return ce
 
 
 class LossFunctionsDerivatives:
-    def squared_error_derivative(a, y):
-        return a - y
+    def squared_error_derivative(y_pred, y_actual):
+        return y_pred - y_actual
+
+    def cross_entropy(y_pred, y_actual, final_layer_name):
+        if final_layer_name == 'SoftmaxActivation':
+            return (y_pred - y_actual) / len(y_pred)

NeuralNetwork.py
@@ -1,118 +1,166 @@
-from typing import List, Any
+from typing import List
+from enum import Enum
 import numpy as np
 import numpy.typing as npt
 from tabulate import tabulate
-from HelperFunctions import ActivationFunctionEnum, ActivationFunctions, LossFunctions, LossFunctionsDerivatives
+from HelperFunctions import LossFunctions, LossFunctionsDerivatives, LossFunctionEnum
+from NeuralNetworkLayer import NeuralNetworkLayer, InputLayer, DenseLayer, DropoutLayer
+from Optimizers import SGD, Optimizer
+from sklearn import metrics
+import os
+import time
 
 
-class NeuralNetworkLayer:
-    no_of_units: int
-    activation_function: ActivationFunctionEnum
-    weights: npt.ArrayLike
-    bias: npt.ArrayLike
-    last_output: npt.ArrayLike
-    last_delta: npt.ArrayLike
-    dropout_type: str
-    dropout_prob: float
-
-    def __init__(self, no_of_units: int,
-                 activation_function: ActivationFunctionEnum,
-                 weights, dropout_type: str = None, dropout_prob: float = 0.2):
-        self.no_of_units = no_of_units
-        self.activation_function = activation_function
-        self.weights = weights
-        if weights is not None:
-            self.bias = np.random.standard_normal((no_of_units, 1))
-            self.dropout_type = dropout_type
-            self.dropout_prob = dropout_prob
+class NeuralNetworkPhase(Enum):
+    TRAIN = 1
+    TEST = 2
 
 
 class NeuralNetwork:
     layers: List[NeuralNetworkLayer]
+    loss_function: LossFunctionEnum
+    phase: NeuralNetworkPhase
+    optimizer: Optimizer
 
     def __init__(self, no_of_inputs: int):
-        input_layer = NeuralNetworkLayer(no_of_inputs, None, None)
+        input_layer = InputLayer(no_of_inputs)
         self.layers = [input_layer]
+        self.model_name = str(time.time())
 
-    def add_hidden_layer(self, no_of_units: int,
-                         activation_function: ActivationFunctionEnum,
-                         dropout_type: str = None, dropout_prob: float = 0.2):
-        self.layers.append(NeuralNetworkLayer(
-            no_of_units, activation_function, np.ones((
-                self.layers[-1].no_of_units, no_of_units)), dropout_type,
-            dropout_prob))
+    def add(self, layer: NeuralNetworkLayer):
+        if isinstance(layer, DenseLayer):
+            last_output = self.layers[0].no_of_inputs
+            for l in reversed(self.layers):
+                if (isinstance(l, DenseLayer)):
+                    last_output = l.no_of_units
+                    break
+            layer.initialise_weights(last_output)
 
-    def describe(self, include_input_layer=False):
-        tabulated_string = [[i, layer.no_of_units,
-                             layer.activation_function.value,
-                             layer.weights.shape[0] * layer.weights.shape[1],
-                             layer.bias.shape[0],
-                             layer.weights.shape[0] * layer.weights.shape[1] +
-                             layer.bias.shape[0]]
-                            for i, layer in enumerate(self.layers)
-                            if layer.weights is not None]
-        if include_input_layer:
-            tabulated_string.insert(0, [0, self.layers[0].no_of_units,
-                                    None, 0, 0, 0])
+        self.layers.append(layer)
+
+    def describe(self):
+        tabulated_string = []
+        for i, layer in enumerate(self.layers):
+            if (isinstance(layer, DenseLayer)):
+                tabulated_string.append(
+                    [i, layer.__class__.__name__, layer.no_of_units, layer.weights.shape[0] * layer.weights.shape[1],
+                     layer.bias.shape[1], layer.weights.shape[0] * layer.weights.shape[1] + layer.bias.shape[1]])
+            elif isinstance(layer, InputLayer):
+                tabulated_string.append(
+                    [i, layer.__class__.__name__, layer.no_of_inputs, '', '', ''])
+            else:
+                tabulated_string.append(
+                    [i, layer.__class__.__name__, '', '', '', ''])
         print(tabulate(tabulated_string, headers=[
-              'Layer no', 'No of units', 'Activation Function',
+              'Layer no', 'Layer type',  'No of units',
               'Weight Parameters', 'Bias Parameters', 'Total Parameters']))
+        tabulated_string = np.array(tabulated_string)
+        tabulated_string[tabulated_string == ''] = '0'
         print('Total Parameters:', np.sum(
             np.array(tabulated_string)[:, 5].astype(int)))
 
-    def display_current_weights(self):
-        for i, layer in enumerate(self.layers[1:]):
-            print('Layer', i+1, '\n', layer.weights, '\n', layer.bias)
-            print('\n')
+    def display_current_weights(self, display_bias=False, display_gradients=False):
+        for i, layer in enumerate(self.layers):
+            if (isinstance(layer, DenseLayer)):
+                print('Layer', i+1, '\nWeights:',
+                      layer.weights.shape, layer.weights, '\n')
+                if display_gradients:
+                    print(
+                        f'Weight gradients: {layer.weight_gradients} {np.max(layer.weight_gradients)=}')
+                if display_bias:
+                    print('Bias:', layer.bias, '\n')
 
     def predict(self, X):
-        return self.forward_pass(X)
+        self.phase = NeuralNetworkPhase.TEST
+        self.forward_pass(X)
+        return np.array(self.layers[-1].last_output)
 
-    def train(self, X, y, epochs, learning_rate=0.01):
-        losses = []
-        for i in range(epochs):
-            y_pred = self.forward_pass(X)
-            self.backward_pass(y, learning_rate)
-            print(f'Epoch {i+1}/{epochs}:')
-            loss = LossFunctions.mean_squared_error(y, y_pred)
-            losses.append(loss)
-            print(f'Loss:{loss}')
-            print(y_pred)
-        return losses
+    def fit(self, X: npt.ArrayLike, y: npt.ArrayLike, epochs, optimizer: Optimizer, batch_size=None):
+        self.optimizer = optimizer
+        self.phase = NeuralNetworkPhase.TRAIN
+        if batch_size is None:
+            batch_size = 32
+        reults = []
+        if y.ndim == 1:
+            y = y.reshape(1, -1)
+
+        for epoch in range(epochs):
+            start = time.time()
+            print(f'Epoch {epoch+1}/{epochs}:')
+            y_preds = []
+            for i in range(0, X.shape[0], batch_size):
+                print(
+                    f'Step {int(i / batch_size)} / {int(X.shape[0] / batch_size)}', end='\r')
+                X_batch = X[i: np.minimum(i + batch_size, X.shape[0])]
+                y_batch = y[i: np.minimum(i + batch_size, X.shape[0])]
+                self.forward_pass(X_batch)
+                y_preds.extend(self.layers[-1].last_output)
+                self.backward_pass(y_batch)
+                self.update_params()
+            y_preds = np.array(y_preds)
+            epoch_results = self.calculate_loss(y, y_preds)
+            epoch_results['time_per_epoch'] = f'{time.time() - start:.2f}'
+            epoch_results['learning_rate'] = f'{self.optimizer.learning_rate:.4f}'
+            print(epoch_results)
+            reults.append(epoch_results)
+            self.optimizer.update_learning_rate()
+        return reults
+
+    def update_params(self):
+        for layer in self.layers:
+            if isinstance(layer, DenseLayer):
+                self.optimizer.update_params(layer)
+
+    def calculate_loss(self, y, y_preds):
+        pred = np.argmax(y_preds, axis=1)
+        y_compare = np.argmax(y, axis=1)
+        score = metrics.accuracy_score(y_compare, pred)
+
+        loss = LossFunctions.cross_entropy(y, y_preds)
+        if self.optimizer.l1_lambda > 0:
+            for layer in self.layers:
+                if isinstance(layer, DenseLayer):
+                    loss += np.sum(np.abs(layer.weights)) * \
+                        self.optimizer.l1_lambda
+        if self.optimizer.l2_lambda > 0:
+            for layer in self.layers:
+                if isinstance(layer, DenseLayer):
+                    loss += np.sum(np.square(layer.weights)) * \
+                        self.optimizer.l2_lambda
+        return {'loss': f'{loss:.3f}', 'accuracy': f'{score:.3f}'}
 
     def forward_pass(self, X):
-        self.layers[0].last_output = X
-        last_layer_output = X
-        for i, layer in enumerate(self.layers[1:]):
-            last_layer_output = np.dot(layer.weights.T, last_layer_output)
-            last_layer_output = last_layer_output + layer.bias
-            last_layer_output = ActivationFunctions.activate(
-                last_layer_output, layer.activation_function)
-            if layer.dropout_type is not None:
-                dp = np.random.rand(*last_layer_output.shape)
-                last_layer_output = (
-                    dp > layer.dropout_prob) * last_layer_output
-            self.layers[i+1].last_output = last_layer_output
-        return last_layer_output
+        for layer in self.layers:
+            if not isinstance(layer, DropoutLayer) or self.phase == NeuralNetworkPhase.TRAIN:
+                layer.forward(X)
+                X = layer.last_output
 
-    def backward_pass(self, y_actual, learning_rate):
+    def backward_pass(self, y_actual):
         output_final_layer = self.layers[-1].last_output
-        delta_final_layer = LossFunctionsDerivatives.squared_error_derivative(output_final_layer, y_actual) * \
-            ActivationFunctions.activation_derivitive(
-                output_final_layer, self.layers[-1].activation_function)
-        self.layers[-1].last_delta = delta_final_layer
-        new_weight_final_layer = self.layers[-1].weights + np.dot(self.layers[-2].last_output,
-                                                                  delta_final_layer.T) * learning_rate
-        new_weights = [new_weight_final_layer]
-        for i in reversed(range(1, len(self.layers) - 1)):
-            delta_next_layer = self.layers[i+1].last_delta
-            weight_next_layer = self.layers[i+1].weights
-            delta_current_layer = np.dot(weight_next_layer, delta_next_layer) * (
-                ActivationFunctions.activation_derivitive(self.layers[i].last_output, self.layers[i].activation_function))
-            self.layers[i].last_delta = delta_current_layer
-            new_weight = self.layers[i].weights + np.dot(
-                self.layers[i-1].last_output, delta_current_layer.T) * learning_rate
-            new_weights.insert(0, new_weight)
+        delta = LossFunctionsDerivatives.cross_entropy(
+            output_final_layer, y_actual, self.layers[-1].__class__.__name__)
+        for layer in reversed(self.layers[1:]):
+            layer.backward(delta)
+            delta = layer.last_delta
 
-        for i, layer in enumerate(self.layers[1:]):
-            layer.weights = new_weights[i]
+    def save_model(self):
+        curr_path = os.path.dirname(os.path.abspath(__file__))
+        model_directory = os.path.join(curr_path, 'models', self.model_name)
+        if not os.path.exists(model_directory):
+            os.makedirs(model_directory)
+        for i, layer in enumerate(self.layers):
+            if isinstance(layer, DenseLayer):
+                np.save(os.path.join(model_directory,
+                        f'weights_{i}'), layer.weights)
+                np.save(os.path.join(model_directory,
+                        f'bias_{i}'), layer.bias)
+
+    def load_weights(self):
+        curr_path = os.path.dirname(os.path.abspath(__file__))
+        model_directory = os.path.join(curr_path, 'models', self.model_name)
+        for i, layer in enumerate(self.layers):
+            if isinstance(layer, DenseLayer):
+                layer.weights = np.load(os.path.join(model_directory,
+                                                     f'weights_{i}.npy'))
+                layer.bias = np.load(os.path.join(model_directory,
+                                                  f'bias_{i}.npy'))